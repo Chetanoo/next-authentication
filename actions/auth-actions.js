5935f30896e52cf8e551668876fde0ff01878c84
KirillGorinDataArt <chetanvse@gmail.com>
2025-07-27 22:53:30 -0300

    user creation

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,5 +1,9 @@
 "use server";
 
+import { createUser } from "@/lib/user";
+import { hashUserPassword } from "@/lib/hash";
+import { redirect } from "next/navigation";
+
 export async function signup(_prevState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -17,4 +21,22 @@ export async function signup(_prevState, formData) {
   if (Object.keys(errors).length) {
     return { errors };
   }
+
+  const hashedPassword = hashUserPassword(password);
+
+  try {
+    createUser(email, hashedPassword);
+  } catch (e) {
+    if (e.code === "SQLITE_CONSTANT_UNIQUE") {
+      return {
+        errors: {
+          email:
+            "The email address is already in use. Please try a different one.",
+        },
+      };
+    }
+    throw e;
+  }
+
+  redirect("/training");
 }
